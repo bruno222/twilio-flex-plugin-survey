6949d772689ae5428e0c680945e410a127dfa94d
Bruno Kilian <bkilian@twilio.com>
2021-09-14 17:38:20 +0200

    add script to create survey tasks.

---
+++ create-task.ts
@@ -0,0 +1,30 @@
+require('dotenv').config();
+import { Twilio } from 'twilio';
+
+const run = async () => {
+  const client = new Twilio(process.env!.TWILIO_ACCOUNT_SID!, process.env!.TWILIO_AUTH_TOKEN!, { edge: 'frankfurt' });
+
+  const workspaceSid = 'WS8137ba265fc37ffc387a36de6d5bc2f5';
+  const workflowSid = 'WWf714a42888675983dbb184056280a9ba';
+
+  const attributes = {
+    name: 'Bob Coletti',
+    journey: 'new car policy',
+    questions: [
+      { stars: 3, title: 'Easy to buy' },
+      { stars: 4, title: 'Pricing' },
+    ],
+    overallStars: 2,
+    details: "I liked but but... the experience of buying was just way too fast... Didn't have the time to enjoy the journey.",
+  };
+
+  const task = await client.taskrouter.workspaces(workspaceSid).tasks.create({
+    taskChannel: 'survey',
+    workflowSid,
+    attributes: JSON.stringify(attributes),
+  });
+
+  console.log('tasks', task);
+};
+
+run();
